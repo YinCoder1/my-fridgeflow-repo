42bf3617ebef1fb11a31c853481767ae97fd8570
YinCoder1 <1431056661@qq.com>
2026-08-01 09:18:38 +0800

    Initial commit

---
+++ src/main/java/com/yin/fridgeflow/FridgeflowApplication.java
@@ -0,0 +1,13 @@
+package com.yin.fridgeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridgeflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FridgeflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yin/fridgeflow/FridgeflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yin.fridgeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FridgeflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
